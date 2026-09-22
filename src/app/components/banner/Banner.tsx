decbfa83687485ab95eeccc81d2c30c1d505dc84
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-22 09:44:14 +0600

    create hero section and import banner img

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import HeroImg from '@/assets/hero_img.jpg'
+import HeroImg from '@/assets/pngwing 1.png';
 
 const Banner = () => {
     return (
@@ -11,12 +11,12 @@ const Banner = () => {
                     className="max-w-lg rounded-lg shadow-2xl"
                 />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">
+                    <h1 className="text-5xl font-bold text-black">Books to freshen up <br /> your bookshelf</h1>
+                    <p className="py-6 text-base-100">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary">View The List</button>
                 </div>
             </div>
         </div>
