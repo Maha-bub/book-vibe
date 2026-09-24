8c534e48d8caa903d4a07e9dfa4e7d98c5c1fa2b
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-24 11:09:13 +0600

    Create books context use layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/sheared/Navbar";
+import ReadBooksContext, { BooksContext } from "@/contex/readBooksContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar></Navbar>
-        {children}</body>
+        <ReadBooksContext>
+          <Navbar></Navbar>
+          <main> {children}</main>
+        </ReadBooksContext>
+      </body>
     </html>
   );
 }
